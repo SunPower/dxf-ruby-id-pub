ba0d04c5a0e641a557e716ac360a5466b90090cd
Nacho Betancourt <trsgarp@gmail.com>
2023-08-16 23:42:10 -0600

    test for line file included

--- test/dxf/parser_rspec.rb
+++ test/dxf/parser_rspec.rb
@@ -2,13 +2,15 @@ require 'dxf/parser'
 
 RSpec.describe DXF::Parser do
   it 'must read file containing Polyline' do
-    parser = File.open('test/fixtures/polyline.dxf', 'r') {|f| DXF::Parser.new.parse(f) }
+    parser = File.open('test/fixtures/AD_include_polyline.dxf', 'r') {|f| DXF::Parser.new.parse(f) }
     expect(parser.entities.length).to eq(6)
-    line = parser.entities.first
-    expect(line).to be_a DXF::Line
+    first = parser.entities.first
+    expect(first).to be_a DXF::Line
   end
-  it 'must read file containing Circle' do
-    parser = File.open('test/fixtures/real-poly.dxf', 'r') {|f| DXF::Parser.new.parse(f) }
-    expect(parser.entities.length).to eq(6)
+  it 'must read file NOT containing Polyline' do
+    parser = File.open('test/fixtures/AD_only_lines.dxf', 'r') {|f| DXF::Parser.new.parse(f) }
+    expect(parser.entities.length).to eq(5)
+    first = parser.entities.first
+    expect(first).to be_a DXF::Line
   end
 end
